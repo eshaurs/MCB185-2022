dfbe1716faae5602c6d79f7faa4c97d08bd28fa3
eshaurs <eshaurs02@gmail.com>
2022-01-23 13:38:41 -0800

    homework update

--- Programs/11sumfac.py
+++ Programs/11sumfac.py
@@ -1,14 +1,19 @@
 #!/usr/bin/env python3
 
-# Write a program that computes the running sum from 1..n
+# Write a program that computes the running sum from 1..n (if n = 5, then sum = 1+2+3+4+5)
 # Also, have it compute the factorial of n while you're at it
 # No, you may not use math.factorial()
 # Use the same loop for both calculations
 
 n = 5
+runningSum = 0
+factorial = 1
 
 # your code goes here
-
+for i in range(1, n+1):
+	runningSum += i
+	factorial *= i
+print(n, runningSum, factorial)
 """
 python3 11sumfac.py
 5 15 120
